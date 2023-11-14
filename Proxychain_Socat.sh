bb7325a368423938bd1d1a71e3ab7edffe1a4a49
RoySean6 <seanx2022@gmail.com>
2023-11-14 14:05:13 +0800

    proxy socat bash v0.3

--- Proxychain_Socat.sh
+++ Proxychain_Socat.sh
@@ -36,50 +36,43 @@ add_new_configuration() {
     check_install "proxychains-ng" "proxychains4" "git clone https://github.com/rofl0r/proxychains-ng.git && cd proxychains-ng && ./configure --prefix=/usr --sysconfdir=/etc && make && sudo make install && cd .. && rm -rf proxychains-ng"
     check_install "supervisor" "supervisorctl" "sudo apt-get update && sudo apt-get install -y supervisor"
 
-    # 获取并解析代理信息
     echo "Enter proxy (format: https://user:password@host:port):"
     read proxy
-    parse_proxy $proxy || return 1
+    parse_proxy $proxy || { echo "Error parsing proxy details. Exiting."; return 1; }
 
-    # 检测代理是否可用
     if ! curl -x $PROXY_TYPE://$PROXY_USER:$PROXY_PASS@$PROXY_HOST:$PROXY_PORT https://cip.cc; then
         echo "Proxy is not working. Exiting."
         return 1
     fi
     echo "Proxy is working."
 
-    # 获取本地端口和目标域名
     echo "Enter local port:"
     read local_port
     echo "Enter target domain (optionally with port, default is 443):"
     read target_domain
 
-    # 如果目标域名不包含端口，则默认为443
     TARGET_HOST=$(echo $target_domain | cut -d':' -f1)
     TARGET_PORT=$(echo $target_domain | cut -s -d':' -f2)
     TARGET_PORT=${TARGET_PORT:-443}
+    CERT_FILE="/root/${TARGET_HOST}.pem"
 
-    # 检查端口是否被占用
     if lsof -i :$local_port
     then
         echo "Port $local_port is already in use. Trying to free it..."
         sudo fuser -k $local_port/tcp
     fi
 
-    # 在root目录生成SSL证书
     cd /root
-    openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=$TARGET_HOST" -keyout server.pem -out server.pem
+    openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=$TARGET_HOST" -keyout $CERT_FILE -out $CERT_FILE
 
-    # 创建一个随机的proxychains配置文件名
     PROXYCHAINS_CONF="/etc/proxychains_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1).conf"
     proxy_conf="socks5  $PROXY_HOST  $PROXY_PORT  $PROXY_USER  $PROXY_PASS"
     echo -e "[ProxyList]\n$proxy_conf" | sudo tee $PROXYCHAINS_CONF
 
-    # 配置supervisor
     supervisor_program_name=$(echo $TARGET_HOST | cut -d'.' -f1)
     supervisor_conf="[program:$supervisor_program_name]
     user=root
-    command=proxychains4 -f $PROXYCHAINS_CONF socat OPENSSL-LISTEN:$local_port,reuseaddr,fork,verify=0,cert=/root/server.pem OPENSSL:$TARGET_HOST:$TARGET_PORT,verify=0
+    command=proxychains4 -f $PROXYCHAINS_CONF socat OPENSSL-LISTEN:$local_port,reuseaddr,fork,verify=0,cert=$CERT_FILE OPENSSL:$TARGET_HOST:$TARGET_PORT,verify=0
     directory=/root
     autorestart=true
     startsecs=10
@@ -87,22 +80,44 @@ add_new_configuration() {
 
     sudo echo "$supervisor_conf" | sudo tee /etc/supervisor/conf.d/$supervisor_program_name.conf
 
-    # 更新并重启特定的supervisor配置
     sudo supervisorctl update
     sudo supervisorctl restart $supervisor_program_name
 
-    echo "Checking if the socat service is working..."
     curl -k -v -H "Host: $TARGET_HOST" https://127.0.0.1:$local_port
 
     echo "New configuration added."
 }
 
 manage_existing_configurations() {
-    # ...
+    echo "Existing supervisor configurations:"
+    ls /etc/supervisor/conf.d/
+
+    read -p "Enter the name of configuration to manage (or enter to cancel): " config_name
+    if [ -z "$config_name" ]; then
+        echo "No configuration selected. Exiting."
+        exit 0
+    fi
+
+    if [ -f "/etc/supervisor/conf.d/$config_name" ]; then
+        echo "Selected configuration: $config_name"
+        read -p "Do you want to delete this configuration? (y/n): " delete_choice
+        if [ "$delete_choice" == "y" ]; then
+            sudo rm "/etc/supervisor/conf.d/$config_name"
+            sudo supervisorctl reread
+            sudo supervisorctl update
+            echo "Configuration $config_name deleted."
+        else
+            echo "Configuration not deleted."
+        fi
+    else
+        echo "Configuration $config_name not found."
+    fi
 }
 
-# 主菜单
-# ...
+echo "Choose an option:"
+echo "1) Add new configuration"
+echo "2) Show and manage existing configurations"
+read -p "Enter your choice (1 or 2): " choice
 
 case $choice in
     1)
